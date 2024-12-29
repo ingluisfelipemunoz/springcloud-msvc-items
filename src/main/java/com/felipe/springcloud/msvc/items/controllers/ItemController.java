f0479d6930362be93c4ec7bc942327577c51a227
Luis Felipe <ingluisfelipemunoz@gmail.com>
2024-12-28 21:40:06 -0400

    returning message on product not found

--- src/main/java/com/felipe/springcloud/msvc/items/controllers/ItemController.java
+++ src/main/java/com/felipe/springcloud/msvc/items/controllers/ItemController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.felipe.springcloud.msvc.items.models.Item;
 import com.felipe.springcloud.msvc.items.services.ItemService;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,11 +27,13 @@ public class ItemController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Item> details(@PathVariable() Long id) {
+    public ResponseEntity<?> details(@PathVariable() Long id) {
         Optional<Item> itemOptional = service.findById(id);
         if (itemOptional.isPresent()) {
             return ResponseEntity.ok(itemOptional.get());
         }
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.status(404)
+                .body(Collections
+                        .singletonMap("message", "Product not found in product microservice"));
     }
 }
