17e9df594a553ece0404a443464b544a56be3eb2
Luis Felipe <ingluisfelipemunoz@gmail.com>
2024-12-28 21:07:38 -0400

    setting up feign client

---
+++ src/main/java/com/felipe/springcloud/msvc/items/clients/ProductFeignClient.java
@@ -0,0 +1,18 @@
+package com.felipe.springcloud.msvc.items.clients;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.felipe.springcloud.msvc.items.models.Product;
+
+@FeignClient(url = "localhost:8001")
+public interface ProductFeignClient {
+    @GetMapping
+    List<Product> findAll();
+
+    @GetMapping("/{id}")
+    public Product getMethodName(@PathVariable Long id);
+}
