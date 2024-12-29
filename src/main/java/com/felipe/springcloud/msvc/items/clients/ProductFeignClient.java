661cc933e204643c6e13ce73b85e3b979640dee7
Luis Felipe <ingluisfelipemunoz@gmail.com>
2024-12-28 22:19:45 -0400

    setting up load balancing

--- src/main/java/com/felipe/springcloud/msvc/items/clients/ProductFeignClient.java
+++ src/main/java/com/felipe/springcloud/msvc/items/clients/ProductFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.felipe.springcloud.msvc.items.models.Product;
 
-@FeignClient(url = "localhost:8001", name = "msvc-products")
+@FeignClient(name = "msvc-products")
 public interface ProductFeignClient {
     @GetMapping
     List<Product> findAll();
