704f8e135a308ab80e02d5432d743422551d439c
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-01-01 10:25:48 -0400

    add Spring Boot Actuator dependency and enable refresh scope in ItemController

--- src/main/java/com/felipe/springcloud/msvc/items/controllers/ItemController.java
+++ src/main/java/com/felipe/springcloud/msvc/items/controllers/ItemController.java
@@ -22,6 +22,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,6 +30,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@RefreshScope
 @RestController
 public class ItemController {
     private final Logger logger = LoggerFactory.getLogger(ItemController.class);
