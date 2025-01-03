fab59798838b09d53c0aec7e4aca7b659d0a1000
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-01-03 12:22:50 -0400

    add CRUD operations for Product in ItemController

--- src/main/java/com/felipe/springcloud/msvc/items/controllers/ItemController.java
+++ src/main/java/com/felipe/springcloud/msvc/items/controllers/ItemController.java
@@ -24,11 +24,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RefreshScope
 @RestController
@@ -124,4 +130,23 @@ public class ItemController {
             return ResponseEntity.ok(new Item(new Product(1L, "Default", 100D, LocalDate.now(), 80), 10));
         });
     }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product create(@RequestBody Product product) {
+        return service.save(product);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product update(@PathVariable Long id, @RequestBody Product product) {
+        return service.update(product, id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+
 }
